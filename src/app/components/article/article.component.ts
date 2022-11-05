f1cc11e1cd21087700da2a7661230bf6ce0ac02c
popastefan10 <popashtefan10@gmail.com>
2022-11-06 01:25:19 +0200

    add create assessment button

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MOCK_ARTICLES } from 'src/app/mock-data/articles';
-import { Article, Section } from '../models/article.model';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-article',
@@ -8,16 +8,12 @@ import { Article, Section } from '../models/article.model';
   styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent implements OnInit {
-  sections: Section[] = [
-      {title: "Capitolul 1", content: ["Lorem ipsum sit dolor et", "Lorem ipsum sit dolor et", "Lorem ipsum sit dolor et"]},
-      {title: "Capitolul 2", content: ["Lorem ipsum sit dolor et", "Lorem ipsum sit dolor et", "Lorem ipsum sit dolor et"]},
-      {title: "Capitolul 3", content: ["Lorem ipsum sit dolor et", "Lorem ipsum sit dolor et", "Lorem ipsum sit dolor et"]},
-  ]
+  public faPlus = faPlus;
+
   public article = MOCK_ARTICLES[0];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
