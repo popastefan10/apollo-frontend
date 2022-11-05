ff9b3cf80e794216593145d53a30adfb6ccfef0c
Andrei Hodoroaga <andreihodoroaga36@gmail.com>
2022-11-05 19:52:03 +0200

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GlobalMaterialModule } from './shared/global-material/global-material.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
