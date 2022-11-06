261de175a83b8c7c428540ae15afe338b1168d89
popastefan10 <popashtefan10@gmail.com>
2022-11-06 12:06:07 +0200

    add second article

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { MOCK_ARTICLES } from 'src/app/mock-data/articles';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
 })
 export class ArticleComponent implements OnInit {
   public faPlus = faPlus;
 
   public article = MOCK_ARTICLES[0];
 
-  constructor(private readonly router: Router) { }
+  constructor(
+    private readonly router: Router,
+    private readonly activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const articleId = this.activatedRoute.snapshot.params['articleId'];
+    this.article =
+      MOCK_ARTICLES[Math.max(0, Math.min(articleId - 1, MOCK_ARTICLES.length))];
   }
 
   goToAssessment(articleId: number) {
