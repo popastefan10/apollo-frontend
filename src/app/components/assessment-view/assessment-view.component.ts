6d08a6a6c2c3b588afebfd21d16bef67b2530839
popastefan10 <popashtefan10@gmail.com>
2022-11-06 04:48:44 +0200

    move question into a separate component

--- src/app/components/assessment-view/assessment-view.component.ts
+++ src/app/components/assessment-view/assessment-view.component.ts
@@ -2,55 +2,106 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 
-interface Question {
-  id: number,
-  title: string,
-  active: boolean,
-  helperQuestions: string[]
+export interface Question {
+  id: number;
+  title: string;
+  active: boolean;
+  helperQuestions: HelperQuestion[];
+}
+
+export interface HelperQuestion {
+  id: number;
+  title: string;
+  active: boolean;
 }
 
 @Component({
   selector: 'app-assessment-view',
   templateUrl: './assessment-view.component.html',
-  styleUrls: ['./assessment-view.component.scss']
+  styleUrls: ['./assessment-view.component.scss'],
 })
 export class AssessmentViewComponent implements OnInit {
   faPlus = faPlus;
   faEllipsisVertical = faEllipsisVertical;
-  questions: Question[] = [{id: 1, title: "What is the area of the earth given the fact that?", active: false, helperQuestions: []}, {id: 2, title: "What color is the sun?", active: true, helperQuestions: []}]
-  constructor() { }
+  questions: Question[] = [
+    {
+      id: 1,
+      title: 'What is the area of the earth given the fact that?',
+      active: false,
+      helperQuestions: [],
+    },
+    {
+      id: 2,
+      title: 'What color is the sun?',
+      active: true,
+      helperQuestions: [],
+    },
+  ];
+
+  public activeQuestionId?: number;
+
+  constructor() {}
+
+  private nextId(): number {
+    return (
+      this.questions.reduce(
+        (prev, crt) =>
+          Math.max(
+            prev,
+            crt.id,
+            crt.helperQuestions.reduce((prev, crt) => Math.max(prev, crt.id), 0)
+          ),
+        0
+      ) + 1
+    );
+  }
 
   ngOnInit(): void {
+    if (this.questions.length > 0) {
+      this.activeQuestionId = this.questions[0].id;
+    }
   }
 
   selectQuestion(questionId: number) {
     // make other questions not active
-    this.questions.forEach(question => {
-      question.active = false;
-    })
-    this.questions.map(question => {
-      if(question.id === questionId)
-        question.active = true;
-      return question;
-    })
+    this.activeQuestionId = questionId;
   }
 
   addQuestion() {
     // make other questions not active
-    this.questions.forEach(question => {
-      question.active = false;
-    })
+    const newId = this.nextId();
+    this.activeQuestionId = newId;
 
     // add new question
-    let newQuestion = {id: this.questions.length + 1, title: "", active: true, helperQuestions: []};
-    this.questions.push(newQuestion)
+    let newQuestion = {
+      id: this.activeQuestionId,
+      title: '',
+      active: true,
+      helperQuestions: [],
+    };
+    this.questions.push(newQuestion);
   }
 
   addHelperQuestion(questionId: number) {
-    console.log(questionId);
+    const newId = this.nextId();
+
+    const newHelperQuestion: HelperQuestion = {
+      id: newId,
+      title: 'new helper',
+      active: true,
+    };
+    this.questions
+      .find((q) => q.id === questionId)
+      ?.helperQuestions.push(newHelperQuestion);
   }
 
   deleteQuestion(questionId: number) {
-    this.questions = this.questions.filter(question => question.id !== questionId);
+    this.questions = this.questions.filter(
+      (question) => question.id !== questionId
+    );
+
+    this.questions.forEach((q) =>
+      q.helperQuestions = q.helperQuestions.filter((helper) => helper.id !== questionId)
+    );
   }
 }
