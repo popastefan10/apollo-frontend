caed8316f91e58030fb10b05d734c9699f10cd38
popastefan10 <popashtefan10@gmail.com>
2022-11-06 06:30:06 +0200

    update question item title

--- src/app/components/assessment-view/assessment-view.component.ts
+++ src/app/components/assessment-view/assessment-view.component.ts
@@ -1,20 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Question } from '../models/question.model';
-
-// export interface Question {
-//   id: number;
-//   title: string;
-//   active: boolean;
-//   helperQuestions: HelperQuestion[];
-// }
-
-// export interface HelperQuestion {
-//   id: number;
-//   title: string;
-//   active: boolean;
-// }
 
 @Component({
   selector: 'app-assessment-view',
@@ -41,7 +29,12 @@ export class AssessmentViewComponent implements OnInit {
     ],
   ];
 
-  public activeQuestionId?: number;
+  public activeQuestion: Question | undefined = this.questions[0][0];
+
+  private activeQuestionIdSubject: BehaviorSubject<number> =
+    new BehaviorSubject(0);
+
+  public activeQuestionId$: Observable<number> = this.activeQuestionIdSubject;
 
   constructor() {}
 
@@ -58,40 +51,56 @@ export class AssessmentViewComponent implements OnInit {
     );
   }
 
+  private findQuestion(questionId: number): Question | undefined {
+    for (let i = 0; i < this.questions.length; i++) {
+      const maybeQ: Question | undefined = this.questions[i].find(
+        (q) => q.id === questionId
+      );
+      if (!!maybeQ) return maybeQ;
+    }
+
+    return undefined;
+  }
+
   ngOnInit(): void {
     if (this.questions.length > 0) {
-      this.activeQuestionId = this.questions[0][0].id;
+      this.activeQuestionIdSubject.next(this.questions[0][0].id);
+      this.activeQuestion = this.questions[0][0];
     }
   }
 
   selectQuestion(questionId: number) {
     // make other questions not active
-    this.activeQuestionId = questionId;
+    this.activeQuestionIdSubject.next(questionId);
+    this.activeQuestion = this.findQuestion(questionId);
   }
 
   addQuestion() {
     // make other questions not active
     const newId = this.nextId();
-    this.activeQuestionId = newId;
+    this.activeQuestionIdSubject.next(newId);
 
     // add new question
     let newQuestion = {
-      id: this.activeQuestionId,
+      id: newId,
       title: '',
       active: true,
       helperQuestions: [],
     };
+    this.activeQuestion = newQuestion;
     this.questions.push([newQuestion]);
   }
 
   addHelperQuestion(questionId: number) {
     const newId = this.nextId();
+    this.activeQuestionIdSubject.next(newId);
 
     const newHelperQuestion: Question = {
       id: newId,
       title: 'new helper',
       active: true,
     };
+    this.activeQuestion = newHelperQuestion;
     this.questions
       .find((qset) => qset[0].id === questionId)
       ?.push(newHelperQuestion);
@@ -102,5 +111,10 @@ export class AssessmentViewComponent implements OnInit {
     // this.questions.forEach((questionSet) =>
     //   questionSet.splice(questionSet.findIndex((q) => q.id === questionId), 1)
     // );
+  }
+
+  public updateSelectedQuestion(q: string): void {
+    if (!!this.activeQuestion)
+      this.activeQuestion.title = q;
   }
 }
